8f35c1a1f5b7ff518a2408d9b0be33bd56d436f9
ZDerkzx <derklevel@gmail.com>
2025-09-09 20:07:26 -0600

    added sysutils

---
+++ lib/sysutils/typenames/supportlenguage.go
@@ -0,0 +1,8 @@
+package typenames
+
+var Support_Lenguage = map[string]string{
+	"PYTHON": ".py",
+	"RUST":   ".rs",
+	"GO":     ".go",
+	"C++":    ".cpp",
+}
---
+++ lib/sysutils/typenames/typeprotocols.go
@@ -0,0 +1,13 @@
+package typenames
+
+/*
+	_______________
+	Aun no aplicado
+	_______________
+
+*/
+var ProtocolsType = map[int8]string{
+	0: "TCP",
+	1: "UDP",
+	2: "HTTP",
+}
