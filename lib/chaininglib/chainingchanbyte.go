90c902494a40df384d8b55e3f4994e0720f2a50e
ZDerkzx <derklevel@gmail.com>
2025-09-17 20:48:17 -0600

    Update chaining function to support []byte

---
+++ lib/chaininglib/chainingchanbyte.go
@@ -0,0 +1,19 @@
+package chaininglib
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*
+encadena todos los paquetes actuales de un chan string en un string
+*/
+func ChainingChanByte(c chan []byte) ([]byte, error) {
+	var strBuilding strings.Builder
+	for b := range c {
+		if _, err := strBuilding.Write(b); err != nil {
+			return []byte{}, fmt.Errorf("error ChainingChanString: Could not write byte in strings.Builder;%w", err)
+		}
+	}
+	return []byte(strBuilding.String()), nil
+}
